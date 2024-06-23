b47c875ab72e0bde39fed74954ce3c7d9212bd58
Bushrajan <darkprince67890@gmail.com>
2024-06-23 13:23:49 +0500

    done

---
+++ main.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const programStart = async (persons) => {
+    let exitProgram = false;
+    do {
+        const ans = await inquirer.prompt([
+            {
+                name: "select",
+                type: "list",
+                message: "Choose an option:",
+                choices: ["Staff", "Student", "Exit"],
+            }
+        ]);
+        if (ans.select == "Staff") {
+            console.log("You approach the staff room. Please feel free to ask any question...");
+        }
+        else if (ans.select == "Student") {
+            const ans = await inquirer.prompt([
+                {
+                    name: "student",
+                    type: "input",
+                    message: "Enter the student's name you wish to engage with:",
+                }
+            ]);
+            const student = persons.students.find(val => val.name == ans.student);
+            if (!student) {
+                const newStudent = new Student(ans.student);
+                persons.addStudent(newStudent);
+                console.log(`Hello, I am ${newStudent.name}. Nice to meet you!`);
+                console.log("New student added.");
+            }
+            else {
+                console.log(`Hello, I am ${student.name}. Nice to see you again!`);
+            }
+            console.log("Current student list:");
+            console.log(persons.students.map(s => s.name));
+        }
+        else if (ans.select == "Exit") {
+            console.log("Exiting the program...");
+            exitProgram = true;
+        }
+    } while (!exitProgram);
+};
+// Example usage:
+const persons = new Person();
+programStart(persons);
